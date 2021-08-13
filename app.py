559feb432ed3004ec3b1e79dbaa56837c80c0476
Mia <mia_bajic@live.com>
2021-08-13 15:54:50 +0100

    added structure, configuration file and example env

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = True
+    CSFR_ENABLED = True
+    SECRET_KEY = 'secret_key'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
