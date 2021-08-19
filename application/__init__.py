d7cdb2163c95da6b6827a196bac49839a5441927
Mia <mia_bajic@live.com>
2021-08-19 21:19:52 +0200

    added comments

--- application/__init__.py
+++ application/__init__.py
@@ -4,6 +4,10 @@ from flask_migrate import Migrate
 
 
 def create_app():
+    """
+    Creates factory function for application.
+    https://flask.palletsprojects.com/en/2.0.x/patterns/appfactories/
+    """
     app = Flask(__name__)
     app.config.from_object(os.environ['APP_SETTINGS'])
 
@@ -11,8 +15,7 @@ def create_app():
     db.init_app(app)
     migrate = Migrate(app, db)
 
-    with app.app_context():
-        from application.views import api
-        app.register_blueprint(api)
+    from application.views import api
+    app.register_blueprint(api)
 
     return app
