fbc153dd9eb5f96f84a0d74a915b66f167f6225e
Mia <mia_bajic@live.com>
2021-09-24 17:37:51 +0200

    updated config

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     DEBUG = True
     CSFR_ENABLED = True
     SECRET_KEY = os.environ['SECRET_KEY']
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'].replace("://", "ql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
