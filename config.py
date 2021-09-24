88eb0be3024a222b81cd9999876398e0e77105b3
Mia <mia_bajic@live.com>
2021-09-24 18:56:51 +0200

    fixed issue with postgres and enum on heroku

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config(object):
     CSFR_ENABLED = True
     SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'].replace("://", "ql://", 1)
+    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
