ef9f6508e1c7e7713b3abcbcbab93c0a6eb27abe
Mia <mia_bajic@live.com>
2021-09-27 22:58:46 +0200

    fixed issue with postgres dialect

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config(object):
     DEBUG = False
     CSFR_ENABLED = True
     SECRET_KEY = os.environ['SECRET_KEY']
-    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'].replace("://", "ql://", 1)
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'].replace("://", "ql://", 1)
+    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
