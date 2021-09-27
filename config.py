d80cd363d9fcb641f74d3a6e749496ff3072ec61
Mia <mia_bajic@live.com>
2021-09-27 22:53:09 +0200

    fixed homepage query

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 import os
 from tempfile import mkdtemp
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    DEBUG = True
+    DEBUG = False
     CSFR_ENABLED = True
     SECRET_KEY = os.environ['SECRET_KEY']
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'].replace("://", "ql://", 1)
-    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'].replace("://", "ql://", 1)
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
